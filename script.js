a9d2c15085c8f317892feecee9dd45066ac1f2f7
BerghFreitas <berg2843@gmail.com>
2023-10-06 22:28:59 -0300

    colocado botao de %

--- script.js
+++ script.js
@@ -12,4 +12,7 @@ function voltar(){
 function calcular(){
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = eval(resultado);
+    
 }
+
+
